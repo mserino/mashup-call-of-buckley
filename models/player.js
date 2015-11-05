adefe384e3faf380ecc08ccf850bfc69c83ad6f3
Serino, Margherita (UK - London) <mserino@deloitte.co.uk>
2015-11-05 21:51:18 +0000

    COB-101 added firebase and trying to save a player

--- models/player.js
+++ models/player.js
@@ -1,11 +1,11 @@
 var Player = function(name) {
 	this.name = name;
-}
+};
 
-Player.prototype.name = {}
+Player.prototype.name = {};
 
-Player.prototype.save = function(){
-	console.log("player saved");
-}
+Player.prototype.save = function(player){
+	database.push({player: player});
+};
 
 module.exports = Player;
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var Player = require('../models/player.js');
+var Firebase = require('firebase');
 
+var database = new Firebase('https://intense-inferno-3591.firebaseio.com/');
 /* GET home page. */
 
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(request, response){
-    var player = new Player(request.body.name)
-    player.save();
+router.post('/name', function(request, response){
+    var player = new Player(request.body.name);
+    // player.save();
+    database.push({player: player});
+});
+
+database.on('value', function(snapshot) {
+	var message = snapshot.val();
+	console.log(snapshot.val());
 });
 
 module.exports = router;
